98729d0777cbe1b0822d4e9c28ad6ca199736cc0
Huan Jiang <seraphjiang@gmail.com>
2022-06-26 13:48:46 -0700

    fix route issue
    Signed-off-by: Huan Jiang <seraphjiang@gmail.com>

--- src/plugins/data_source_management/public/components/app.tsx
+++ src/plugins/data_source_management/public/components/app.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FormattedMessage, I18nProvider } from '@osd/i18n/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import {
   EuiButton,
@@ -49,59 +49,65 @@ export const DataSourceManagementApp = ({ services }: DataSourceManagmentContext
   return (
     <Router>
       <I18nProvider>
-        <>
-          <EuiPage restrictWidth="1000px">
-            <EuiPageBody component="main">
-              <EuiPageHeader>
-                <EuiTitle size="l">
-                  <h1>
-                    <FormattedMessage
-                      id="dataSourceManagement.helloWorldText"
-                      defaultMessage="{name}"
-                      values={{ name: PLUGIN_NAME }}
-                    />
-                  </h1>
-                </EuiTitle>
-              </EuiPageHeader>
-              <EuiPageContent>
-                <EuiPageContentHeader>
-                  <EuiTitle>
-                    <h2>
-                      <FormattedMessage
-                        id="dataSourceManagement.congratulationsTitle"
-                        defaultMessage="Congratulations, you have successfully created a new OpenSearch Dashboards Plugin!"
-                      />
-                    </h2>
-                  </EuiTitle>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="dataSourceManagement.content"
-                        defaultMessage="Look through the generated code and check out the plugin development documentation."
-                      />
-                    </p>
-                    <EuiHorizontalRule />
-                    <p>
-                      <FormattedMessage
-                        id="dataSourceManagement.timestampText"
-                        defaultMessage="Last timestamp: {time}"
-                        values={{ time: timestamp ? timestamp : 'Unknown' }}
-                      />
-                    </p>
-                    <EuiButton type="primary" size="s" onClick={onClickHandler}>
-                      <FormattedMessage
-                        id="dataSourceManagement.buttonText"
-                        defaultMessage="Get data"
-                      />
-                    </EuiButton>
-                  </EuiText>
-                </EuiPageContentBody>
-              </EuiPageContent>
-            </EuiPageBody>
-          </EuiPage>
-        </>
+        <Router>
+          <Switch>
+            <Route path={['/']}>
+              <>
+                <EuiPage restrictWidth="1000px">
+                  <EuiPageBody component="main">
+                    <EuiPageHeader>
+                      <EuiTitle size="l">
+                        <h1>
+                          <FormattedMessage
+                            id="dataSourceManagement.helloWorldText"
+                            defaultMessage="{name}"
+                            values={{ name: PLUGIN_NAME }}
+                          />
+                        </h1>
+                      </EuiTitle>
+                    </EuiPageHeader>
+                    <EuiPageContent>
+                      <EuiPageContentHeader>
+                        <EuiTitle>
+                          <h2>
+                            <FormattedMessage
+                              id="dataSourceManagement.congratulationsTitle"
+                              defaultMessage="Congratulations, you have successfully created a new OpenSearch Dashboards Plugin!"
+                            />
+                          </h2>
+                        </EuiTitle>
+                      </EuiPageContentHeader>
+                      <EuiPageContentBody>
+                        <EuiText>
+                          <p>
+                            <FormattedMessage
+                              id="dataSourceManagement.content"
+                              defaultMessage="Look through the generated code and check out the plugin development documentation."
+                            />
+                          </p>
+                          <EuiHorizontalRule />
+                          <p>
+                            <FormattedMessage
+                              id="dataSourceManagement.timestampText"
+                              defaultMessage="Last timestamp: {time}"
+                              values={{ time: timestamp ? timestamp : 'Unknown' }}
+                            />
+                          </p>
+                          <EuiButton type="primary" size="s" onClick={onClickHandler}>
+                            <FormattedMessage
+                              id="dataSourceManagement.buttonText"
+                              defaultMessage="Get data"
+                            />
+                          </EuiButton>
+                        </EuiText>
+                      </EuiPageContentBody>
+                    </EuiPageContent>
+                  </EuiPageBody>
+                </EuiPage>
+              </>
+            </Route>
+          </Switch>
+        </Router>
       </I18nProvider>
     </Router>
   );
